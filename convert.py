c9c7b3ab65752b16830a3d9931ef887615c9c336
Mike Devlin <mike@epay3.com>
2023-02-27 21:35:35 -0600

    Fix directory listing; read key/value pairs from form recognizer

--- convert.py
+++ convert.py
@@ -1,23 +1,21 @@
 import os
 import fitz
 
+for file in os.listdir('./pdf'): 
+    if os.path.isfile(f"./pdf/{file}") and file.endswith('.pdf'):
+        print(file)
+        doc = fitz.open(f"./pdf/{file}")
 
-for (dirpath, dirnames, filenames) in os.walk('./'):   
-    for file in filenames:
-        if file.endswith('.pdf'):
-            print(file)
-            doc = fitz.open(file)
+        index = 1
+        dir_name = f"img"
+        if os.path.exists(dir_name) == False:
+            os.mkdir(dir_name)
 
-            index = 1
-            dir_name = f"img"
-            if os.path.exists(dir_name) == False:
-                os.mkdir(dir_name)
+        for page in doc:
+            matrix = fitz.Matrix(2, 2)
+            pix = page.get_pixmap(matrix=matrix)
+            output = f"{dir_name}/{file}-page-{index:03}.png"
+            pix.save(output)
+            index += 1
 
-            for page in doc:
-                matrix = fitz.Matrix(2, 2)
-                pix = page.get_pixmap(matrix=matrix)
-                output = f"{dir_name}/{file}-page-{index:03}.png"
-                pix.save(output)
-                index += 1
-
-            doc.close()
+        doc.close()
--- form_recognizer.py
+++ form_recognizer.py
@@ -18,19 +18,20 @@ if os.path.exists("out") == False:
 
 with open(r"./out/output-formrecognizer.md", "w") as output_file:
     page = 1
-    for (dirpath, dirnames, filenames) in walk('./img'):
-        for file in filenames:
-            if file.endswith('.png'):
-                with open(f"img/{file}", "rb") as fd:
-                    print(f"Parsing {file}...")
-                    document = fd.read()
-                    poller = document_analysis_client.begin_analyze_document("prebuilt-layout", document)
-                    result = poller.result()
-                    if len(result.paragraphs) > 0:
-                        output_file.write(f"___\n(page {page})\n\n")
-                        for paragraph in result.paragraphs:
-                            output_file.write(paragraph.content + "\n\n")
-                page += 1
+    for file in os.listdir('./img'):
+        if os.path.isfile(f"./img/{file}") and file.endswith('.png'):
+            with open(f"img/{file}", "rb") as fd:
+                print(f"Parsing {file}...")
+                document = fd.read()
+                poller = document_analysis_client.begin_analyze_document("prebuilt-document", document)
+                result = poller.result()
+                for kv_pair in result.key_value_pairs:
+                    if kv_pair.key and kv_pair.value:
+                        print("* Key '{}': Value: '{}'".format(kv_pair.key.content, kv_pair.value.content))
+                    else:
+                        print("* Key '{}': Value:".format(kv_pair.key.content))
+            page += 1
+            break
     output_file.close()                    
 
 
